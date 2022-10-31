70d928c036284c1b8ecc43ee0fe8d61f0730a682
szymicorp <szymicorp@gmail.com>
2022-10-31 13:32:40 +0100

    test: UTXOStorageTest

--- src/test/java/net/ddns/protocoin/service/database/UTXOStorageTest.java
+++ src/test/java/net/ddns/protocoin/service/database/UTXOStorageTest.java
@@ -1,137 +1,149 @@
 package net.ddns.protocoin.service.database;
 
-import net.ddns.protocoin.core.blockchain.Blockchain;
-import net.ddns.protocoin.core.blockchain.data.Satoshi;
+import net.ddns.protocoin.core.blockchain.data.Bytes;
 import net.ddns.protocoin.core.blockchain.transaction.Transaction;
 import net.ddns.protocoin.core.blockchain.transaction.TransactionInput;
 import net.ddns.protocoin.core.blockchain.transaction.TransactionOutput;
 import net.ddns.protocoin.core.blockchain.transaction.signature.LockingScript;
-import net.ddns.protocoin.core.blockchain.transaction.signature.PayToPubKeyHash;
 import net.ddns.protocoin.core.blockchain.transaction.signature.ScriptSignature;
-import net.ddns.protocoin.core.ecdsa.Curve;
-import net.ddns.protocoin.core.ecdsa.ECPoint;
 import net.ddns.protocoin.core.script.ScriptInterpreter;
-import net.ddns.protocoin.core.util.ArrayUtil;
 import net.ddns.protocoin.core.util.Converter;
 import net.ddns.protocoin.core.util.Hash;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collections;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class UTXOStorageTest {
-    private ScriptInterpreter scriptInterpreter;
     private UTXOStorage utxoStorage;
+
+    private TransactionOutput transactionOutputMock1;
+    private byte[] publicKeyHashBytes1;
+
+    private TransactionOutput transactionOutputMock2;
+    private Bytes publicKeyBytes2;
+    private byte[] publicKeyHashBytes2;
+    private byte[] txid2;
 
     @BeforeEach
     void setup(){
-        this.scriptInterpreter = mock(ScriptInterpreter.class);
+        var scriptInterpreter = mock(ScriptInterpreter.class);
         utxoStorage = new UTXOStorage(scriptInterpreter);
-    }
 
-    @Test
-    void shouldAddUnspentTransactionOutput(){
-        var publicKeyMock = new ECPoint(new BigInteger("661BA57FED0D115222E30FE7E9509325EE30E7E284D3641E6FB5E67368C2DB18", 16), new BigInteger("5ADA8EFC5DC43AF6BF474A41ED6237573DC4ED693D49102C42FFC88510500799", 16));
-        var publicKeyHash = "D0ECF5C0DCF3797201379ED48F2DE37002FE823C"; //Hash.ripeMD160(Hash.sha256(publicKeyMock.toByteArray()));
-        var lockingScript = PayToPubKeyHash.fromPublicKey(publicKeyMock.toByteArray());
-        var transactionOutput1 = new TransactionOutput(Satoshi.valueOf(10.0),lockingScript);
-        var transactionOutput2 = new TransactionOutput(Satoshi.valueOf(20.0),lockingScript);
+        transactionOutputMock1 = mock(TransactionOutput.class);
+        var lockingScriptMock1 = mock(LockingScript.class);
+        var publicKeyBytes1 = Bytes.of(Converter.hexStringToByteArray("01".repeat(64)), 64);
+        publicKeyHashBytes1 = Hash.ripeMD160(Hash.sha256(publicKeyBytes1.getBytes()));
+        var parentMock1 = mock(Transaction.class);
+        byte[] txid1 = new byte[]{0};
+        when(transactionOutputMock1.getLockingScript()).thenReturn(lockingScriptMock1);
+        when(transactionOutputMock1.getVout()).thenReturn(Bytes.of(new byte[1], 1));
+        when(lockingScriptMock1.getReceiver()).thenReturn(publicKeyHashBytes1);
+        when(transactionOutputMock1.getParent()).thenReturn(parentMock1);
+        when(parentMock1.getTxId()).thenReturn(txid1);
 
-        var transactionOutputs = new ArrayList<TransactionOutput>();
-        transactionOutputs.add(transactionOutput1);
-        transactionOutputs.add(transactionOutput2);
-
-        utxoStorage.addUnspentTransactionOutput(transactionOutput1);
-        utxoStorage.addUnspentTransactionOutput(transactionOutput2);
-
-        var received = utxoStorage.getUTXOs(Converter.hexStringToByteArray(publicKeyHash));
-
-        assertEquals(2, received.size());
-        for(TransactionOutput output : transactionOutputs){
-            assertTrue(received.contains(output));
-        }
-    }
-
-    @Test
-    void shouldSpendTransactionOutput() {
-        var privateKeyMock1 = new BigInteger("D12D2FACA9AD92828D89683778CB8DFCCDBD6C9E92F6AB7D6065E8AACC1FF6D6", 16);
-        var publicKeyHex1 = "661BA57FED0D115222E30FE7E9509325EE30E7E284D3641E6FB5E67368C2DB185ADA8EFC5DC43AF6BF474A41ED6237573DC4ED693D49102C42FFC88510500799";
-        var publicKeyHex2 = "6E17F61F5A05F10A29CF5ECB638069F9A0DB0F97753D046EBB6EA41055FB5316E7FE027B9934A6A31C65E95142BA2678E9045A46F18B12DC8000F73DECCF22B7";
-
-        var publicKey1 = new BigInteger(publicKeyHex1, 16).toByteArray();
-        var publicKey2 = new BigInteger(publicKeyHex2, 16).toByteArray();
-
-        var publicKey1Hash = Hash.ripeMD160(Hash.sha256(publicKey1));
-        var publicKey2Hash = Hash.ripeMD160(Hash.sha256(publicKey2));
-        var blockchain = new Blockchain(publicKey1);
-
-        var topBlock = blockchain.getTopBlock();
-
-        var transactionInput = new TransactionInput(
-                topBlock.getTransactions().get(0).getTxId(),
-                ArrayUtil.newByteArrayPaddedWithZeros(4, BigInteger.ZERO.toByteArray())
-        );
-        var transactionOutput = new TransactionOutput(
-                Satoshi.valueOf(10.0),
-                PayToPubKeyHash.fromPublicKey(publicKey2)
-        );
-        var transaction = new Transaction(
-                Collections.singletonList(transactionInput),
-                Collections.singletonList(transactionOutput)
-        );
-
-        var transactionHash = Hash.sha256(transaction.getBytesWithoutSignatures());
-        var signature = Curve.secp256k1.sign(privateKeyMock1, transactionHash);
-
-        transactionInput.setScriptSignature(new ScriptSignature(signature.getBytes(), publicKey1));
-
-        utxoStorage.addUnspentTransactionOutput(topBlock.getTransactions().get(0).getTransactionOutputs().get(0));
-        assertEquals(1, utxoStorage.getUTXOs(publicKey1Hash).size());
-        assertEquals(0, utxoStorage.getUTXOs(publicKey2Hash).size());
-
-        utxoStorage.spendTransactionOutput(transactionInput);
-        assertEquals(0, utxoStorage.getUTXOs(publicKey1Hash).size());
-        assertEquals(0, utxoStorage.getUTXOs(publicKey2Hash).size());
-
-        utxoStorage.addUnspentTransactionOutput(transactionOutput);
-        assertEquals(0, utxoStorage.getUTXOs(publicKey1Hash).size());
-        assertEquals(1, utxoStorage.getUTXOs(publicKey2Hash).size());
+        transactionOutputMock2 = mock(TransactionOutput.class);
+        var lockingScriptMock2 = mock(LockingScript.class);
+        publicKeyBytes2 = Bytes.of(Converter.hexStringToByteArray("10".repeat(64)), 64);
+        publicKeyHashBytes2 = Hash.ripeMD160(Hash.sha256(publicKeyBytes2.getBytes()));
+        var parentMock2 = mock(Transaction.class);
+        txid2 = new byte[]{1};
+        when(transactionOutputMock2.getLockingScript()).thenReturn(lockingScriptMock2);
+        when(transactionOutputMock2.getVout()).thenReturn(Bytes.of(new byte[1], 1));
+        when(lockingScriptMock2.getReceiver()).thenReturn(publicKeyHashBytes2);
+        when(transactionOutputMock2.getParent()).thenReturn(parentMock2);
+        when(parentMock2.getTxId()).thenReturn(txid2);
     }
 
     @Test
     void shouldRegisterOutput() {
-        // given:
-        var transactionOutputMock = mock(TransactionOutput.class);
-        var lockingScriptMock = mock(LockingScript.class);
-        var receiverBytes = new byte[20];
-        when(transactionOutputMock.getLockingScript()).thenReturn(lockingScriptMock);
-        when(lockingScriptMock.getReceiver()).thenReturn(receiverBytes);
-
         // when:
-        utxoStorage.addUnspentTransactionOutput(transactionOutputMock);
-        var utxos = utxoStorage.getUTXOs(receiverBytes);
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock1);
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock2);
 
         // then:
-        assertEquals(utxos.size(), 1);
-        assertEquals(utxos.get(0), transactionOutputMock);
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes1).size());
+        assertEquals(transactionOutputMock1, utxoStorage.getUTXOs(publicKeyHashBytes1).get(0));
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes2).size());
+        assertEquals(transactionOutputMock2, utxoStorage.getUTXOs(publicKeyHashBytes2).get(0));
     }
 
     @Test
     void shouldSpendOutput() {
+        // given:
         var transactionInputMock = mock(TransactionInput.class);
         var scriptMock = mock(ScriptSignature.class);
         when(transactionInputMock.getScriptSignature()).thenReturn(scriptMock);
+        when(transactionInputMock.getVout()).thenReturn(Bytes.of(new byte[1], 1));
+        when(scriptMock.getPublicKey()).thenReturn(publicKeyBytes2);
+        when(transactionInputMock.getTxid()).thenReturn(Bytes.of(txid2, txid2.length));
+
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock1);
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock2);
+        utxoStorage.spendTransactionOutput(transactionInputMock);
+
+        // then:
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes1).size());
+        assertEquals(transactionOutputMock1, utxoStorage.getUTXOs(publicKeyHashBytes1).get(0));
+        assertEquals(0, utxoStorage.getUTXOs(publicKeyHashBytes2).size());
     }
 
     @Test
-    void shouldVerifyCorrectTransaction() {
+    void shouldNotSpendOutputWhenDifferentPublicKey() {
+        // given:
+        var transactionInputMock = mock(TransactionInput.class);
+        var scriptMock = mock(ScriptSignature.class);
+        when(transactionInputMock.getScriptSignature()).thenReturn(scriptMock);
+        when(transactionInputMock.getVout()).thenReturn(Bytes.of(new byte[1], 1));
+        when(scriptMock.getPublicKey()).thenReturn(Bytes.of(new byte[64], 64));
+        when(transactionInputMock.getTxid()).thenReturn(Bytes.of(txid2, txid2.length));
 
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock1);
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock2);
+        utxoStorage.spendTransactionOutput(transactionInputMock);
+
+        // then:
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes1).size());
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes2).size());
+    }
+
+    @Test
+    void shouldNotSpendOutputWhenDifferentTxid() {
+        // given:
+        var transactionInputMock = mock(TransactionInput.class);
+        var scriptMock = mock(ScriptSignature.class);
+        when(transactionInputMock.getScriptSignature()).thenReturn(scriptMock);
+        when(transactionInputMock.getVout()).thenReturn(Bytes.of(new byte[1], 1));
+        when(scriptMock.getPublicKey()).thenReturn(publicKeyBytes2);
+        when(transactionInputMock.getTxid()).thenReturn(Bytes.of(new byte[0], 0));
+
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock1);
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock2);
+        utxoStorage.spendTransactionOutput(transactionInputMock);
+
+        // then:
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes1).size());
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes2).size());
+    }
+
+    @Test
+    void shouldNotSpendOutputWhenDifferentVout() {
+        // given:
+        var transactionInputMock = mock(TransactionInput.class);
+        var scriptMock = mock(ScriptSignature.class);
+        when(transactionInputMock.getScriptSignature()).thenReturn(scriptMock);
+        when(transactionInputMock.getVout()).thenReturn(Bytes.of(new byte[]{1}, 1));
+        when(scriptMock.getPublicKey()).thenReturn(publicKeyBytes2);
+        when(transactionInputMock.getTxid()).thenReturn(Bytes.of(txid2, txid2.length));
+
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock1);
+        utxoStorage.addUnspentTransactionOutput(transactionOutputMock2);
+        utxoStorage.spendTransactionOutput(transactionInputMock);
+
+        // then:
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes1).size());
+        assertEquals(1, utxoStorage.getUTXOs(publicKeyHashBytes2).size());
     }
 }
